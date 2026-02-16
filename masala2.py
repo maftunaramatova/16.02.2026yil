18ba25afa8bd2bf196623a4250ade2f1f1e8042f
senyoramoon010 <djumaniyazovumarjon@gmail.com>
2026-02-16 17:52:54 +0500

    Add files via upload

---
+++ masala2.py
@@ -0,0 +1,60 @@
+# ##1-masala:
+# class Salomlashish:
+#     def __init__(self,ism,familya):
+#         self.ism = ism
+#         self.familya = familya
+#
+#     def salomlash(self):
+#         return f"Assalomu aleykum.{self.ism} {self.familya}.Jamoamizga xush kelibsiz !"
+#
+#
+# a = Salomlashish("Zuhra","Otanazarova")
+# print(a.salomlash())
+# b = Salomlashish("Ruxshona","Jo'rabekova")
+# print(b.salomlash())
+# c = Salomlashish("Marjona","Setmamatova")
+# print(c.salomlash())
+
+# ##2-masala:
+# class Salomlashish:
+#     def __init__(self,ism,fam):
+#         self.ism = ism
+#         self.__fam = fam
+#
+#     def get_fam(self):
+#         return self.__fam
+#
+# salom1 = Salomlashish("Ali","Valiyev")
+# salom2 = Salomlashish("G'anijon","Norimov")
+# salom3 = Salomlashish("Bobur","Jovliyev")
+#
+# print(salom1.get_fam)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
